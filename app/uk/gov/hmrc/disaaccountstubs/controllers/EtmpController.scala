1073397b290a2feb3c01d622525e78ba1225a2dd
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2026-08-14 14:34:34 +0100

    DFI-2332 new endpoint to stub ETMP get registration details

---
+++ app/uk/gov/hmrc/disaaccountstubs/controllers/EtmpController.scala
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.disaaccountstubs.controllers
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.disaaccountstubs.controllers.EtmpController.NotFoundZref
+import uk.gov.hmrc.disaaccountstubs.models.YesNoAnswer
+import uk.gov.hmrc.disaaccountstubs.models.journeyData.certificatesofauthority.{CertificatesOfAuthority, CertificatesOfAuthorityYesNo, FcaArticles, FinancialOrganisation}
+import uk.gov.hmrc.disaaccountstubs.models.journeyData.isaProducts.{InnovativeFinancialProduct, IsaProduct, IsaProducts}
+import uk.gov.hmrc.disaaccountstubs.models.journeyData.liaisonofficers.{LiaisonOfficer, LiaisonOfficerCommunication, LiaisonOfficers}
+import uk.gov.hmrc.disaaccountstubs.models.journeyData.orgdetails.OrganisationDetails
+import uk.gov.hmrc.disaaccountstubs.models.journeyData.signatories.{Signatories, Signatory}
+import uk.gov.hmrc.disaaccountstubs.models.journeyData.thirdparty.{ThirdParty, ThirdPartyOrganisations}
+import uk.gov.hmrc.disaaccountstubs.models.journeyData.{BusinessVerification, CorrespondenceAddress, JourneyData, OrganisationEmail}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import java.time.Instant
+import java.util.UUID
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class EtmpController @Inject() (
+  cc: ControllerComponents
+) extends BackendController(cc)
+    with Logging {
+
+  def retrieveRegistrationDetails(zref: String): Action[AnyContent] = Action { implicit request =>
+    zref match {
+      case NotFoundZref =>
+        logger.info(s"Returning registration details not found response for zref: [$zref]")
+        NotFound
+
+      case _ =>
+        logger.info(s"Returning registration details success response for zref: [$zref]")
+        Ok(Json.toJson(registrationDetails(zref)))
+    }
+  }
+
+  private def registrationDetails(zref: String): JourneyData =
+    JourneyData(
+      groupId = UUID.randomUUID().toString,
+      businessVerification = Some(
+        BusinessVerification(
+          ctUtr = Some("1234567890"),
+          companyName = Some("Test Isa Manager Ltd"),
+          companyNumber = Some("12345678"),
+          businessPartnerId = Some("XA0001234567890")
+        )
+      ),
+      organisationDetails = Some(
+        OrganisationDetails(
+          zRefNumber = Some(zref),
+          tradingName = Some("Test Trading Name"),
+          fcaNumber = Some("123456"),
+          correspondenceAddress = Some(
+            CorrespondenceAddress(
+              addressLine1 = Some("1 Test Street"),
+              addressLine2 = Some("Test Town"),
+              addressLine3 = Some("Test County"),
+              postCode = Some("AA1 1AA")
+            )
+          ),
+          orgTelephoneNumber = Some("01234567890")
+        )
+      ),
+      organisationEmail = Some(OrganisationEmail(organisationEmail = Some("test@example.com"), verified = Some(true))),
+      isaProducts = Some(
+        IsaProducts(
+          isaProducts = Some(Seq(IsaProduct.CashIsas, IsaProduct.StocksAndSharesIsas)),
+          innovativeFinancialProducts = Some(Seq(InnovativeFinancialProduct.CrowdFundedDebentures)),
+          p2pPlatform = None,
+          p2pPlatformNumber = None
+        )
+      ),
+      certificatesOfAuthority = Some(
+        CertificatesOfAuthority(
+          certificatesYesNo = Some(CertificatesOfAuthorityYesNo.Yes),
+          fcaArticles = Some(Seq(FcaArticles.Article36H)),
+          financialOrganisation = Some(Seq(FinancialOrganisation.Bank))
+        )
+      ),
+      liaisonOfficers = Some(
+        LiaisonOfficers(
+          liaisonOfficers = Seq(
+            LiaisonOfficer(
+              id = UUID.randomUUID().toString,
+              fullName = Some("Test Officer"),
+              phoneNumber = Some("01234567890"),
+              communication = Set(LiaisonOfficerCommunication.ByEmail),
+              email = Some("officer@example.com")
+            )
+          )
+        )
+      ),
+      signatories = Some(
+        Signatories(
+          signatories = Seq(
+            Signatory(id = UUID.randomUUID().toString, fullName = Some("Test Signatory"), jobTitle = Some("Director"))
+          )
+        )
+      ),
+      thirdPartyOrganisations = Some(
+        ThirdPartyOrganisations(
+          managedByThirdParty = Some(YesNoAnswer.No),
+          thirdParties = Seq.empty[ThirdParty],
+          connectedOrganisations = Seq.empty[String]
+        )
+      ),
+      lastUpdated = Some(Instant.now())
+    )
+}
+
+object EtmpController {
+  private val NotFoundZref = "Z0404"
+}
---
+++ test/controllers/EtmpControllerSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import uk.gov.hmrc.disaaccountstubs.controllers.routes.EtmpController
+import uk.gov.hmrc.disaaccountstubs.models.journeyData.JourneyData
+
+class EtmpControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
+
+  "EtmpController.retrieveRegistrationDetails" should {
+
+    "return 200 with a JourneyData payload containing the requested zref" in {
+      val zref    = "Z1234"
+      val request = FakeRequest(GET, EtmpController.retrieveRegistrationDetails(zref).url)
+
+      val result = route(app, request).get
+
+      status(result) mustBe OK
+      contentType(result) mustBe Some("application/json")
+
+      val journeyData = contentAsJson(result).as[JourneyData]
+      journeyData.organisationDetails.flatMap(_.zRefNumber) mustBe Some(zref)
+    }
+
+    "return 404 when the zref is not found" in {
+      val request = FakeRequest(GET, EtmpController.retrieveRegistrationDetails("Z0404").url)
+
+      val result = route(app, request).get
+
+      status(result) mustBe NOT_FOUND
+    }
+  }
+}
