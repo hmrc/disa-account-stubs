cf7fbee552538f2a0a7d374f5d669f6bcd00b3ab
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2026-09-15 16:40:18 +0100

    DFI-2475 added InnovativeFinanceIsas to ISA products page for testing P2P scenarios

--- app/uk/gov/hmrc/disaaccountstubs/controllers/EtmpController.scala
+++ app/uk/gov/hmrc/disaaccountstubs/controllers/EtmpController.scala
@@ -52,6 +52,7 @@ class EtmpController @Inject() (
         logger.info(
           s"[EtmpController][retrieveRegistrationDetails] Returning registration details success response for zref: [$zref]"
         )
+        println(Json.toJson(registrationDetails(zref)))
         Ok(Json.toJson(registrationDetails(zref)))
     }
   }
@@ -86,7 +87,7 @@ class EtmpController @Inject() (
       organisationEmail = Some(OrganisationEmail(organisationEmail = Some("test@example.com"), verified = Some(true))),
       isaProducts = Some(
         IsaProducts(
-          isaProducts = Some(Seq(IsaProduct.CashIsas, IsaProduct.StocksAndSharesIsas)),
+          isaProducts = Some(Seq(IsaProduct.CashIsas, IsaProduct.StocksAndSharesIsas, IsaProduct.InnovativeFinanceIsas)),
           innovativeFinancialProducts = Some(Seq(InnovativeFinancialProduct.CrowdFundedDebentures)),
           p2pPlatform = None,
           p2pPlatformNumber = None
